4a4953506dee7bb45229d460bc9df3110181faa3
Sachin.Varghese <Sachin.Varghese@mu-sigma.com>
2018-09-08 16:44:02 +0530

    update prediction

--- predict.js
+++ predict.js
@@ -1,9 +1,12 @@
 const MyModel = require("./MyModel");
 
 let x = new MyModel();
-x.init().then(() => {
-  x.predict([
+x.init().then(async () => {
+ val= x.predict([
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [11, 2, 3, 4, 5, 6, 7, 8, 9, 11]
-  ]).print();
+  ])
+prediction = await val.data();
+console.log(prediction,val.shape);
+	
 });
